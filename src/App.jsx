3314b401f3a51c12e4305dd6d99c8e1395747c05
Mouneesh116 <mgangadhari@loyaltymethods.com>
2025-08-29 15:56:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,6 @@ function App() {
           count is {count}
         </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
